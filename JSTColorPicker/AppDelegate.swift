0870a6a0854dd8f4f09e0c7a8fc3022559462a76
i_82 <5410705+Lessica@users.noreply.github.com>
2020-01-12 17:02:08 +0800

    Initial Commit

---
+++ JSTColorPicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSTColorPicker
+//
+//  Created by Darwin on 1/12/20.
+//  Copyright © 2020 JST. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSTColorPicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSTColorPicker
+//
+//  Created by Darwin on 1/12/20.
+//  Copyright © 2020 JST. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
