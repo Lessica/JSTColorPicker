7cc816d5522c546158ecb6f760bd698537ec876a
i_82 <5410705+Lessica@users.noreply.github.com>
2020-02-06 16:36:18 +0800

    [Sidebar] use split view

---
+++ JSTColorPicker/Sidebar/SidebarSplitView.swift
@@ -0,0 +1,13 @@
+//
+//  SidebarSplitView.swift
+//  JSTColorPicker
+//
+//  Created by Darwin on 2/6/20.
+//  Copyright © 2020 JST. All rights reserved.
+//
+
+import Cocoa
+
+class SidebarSplitView: NSSplitView {
+    
+}
