f92754c1701d831cbb00ae3bd539a1e14cc2b3a1
i_82 <5410705+Lessica@users.noreply.github.com>
2020-03-13 20:53:09 +0800

    [PixelMatch] multi-thread optimized

--- PixelMatch/main.swift
+++ PixelMatch/main.swift
@@ -14,9 +14,23 @@ public extension Collection {
     }
 }
 
+enum PixelMatchCommandLineError: LocalizedError {
+    case fileDoesNotExist(url: URL)
+    case sizesDoNotMatch(size1: CGSize, size2: CGSize)
+    
+    var failureReason: String? {
+        switch self {
+        case let .fileDoesNotExist(url):
+            return String(format: NSLocalizedString("File does not exist: %@", comment: "PixelMatchCommandLineError"), url.path)
+        case let .sizesDoNotMatch(size1, size2):
+            return String(format: NSLocalizedString("Image sizes do not match: %dx%d vs %dx%d", comment: "PixelMatchCommandLineError"), size1.width, size1.height, size2.width, size2.height)
+        }
+    }
+}
+
 let args = CommandLine.arguments
 guard args.count >= 4 else {
-    print("Usage: pixelmatch image1.png image2.png diff.png [threshold=0.005] [includeAA=false]")
+    print("usage: pixelmatch image1.png image2.png diff.png [threshold=0.005] [includeAA=false]")
     exit(EXIT_FAILURE)
 }
 
@@ -29,66 +43,107 @@ let img1Path = URL(fileURLWithPath: (args[safe: 1] ?? ""), relativeTo: nil)
 var options = MatchOptions()
 if threshold != nil { options.threshold = CGFloat(threshold!) }
 options.includeAA = includeAA
+dump(options)
 
-let img1 = JSTPixelImage(nsImage: NSImage(contentsOf: img1Path)!)
-let img2 = JSTPixelImage(nsImage: NSImage(contentsOf: img2Path)!)
+
+// MARK: - Process Begin
+
+let startTime = CFAbsoluteTimeGetCurrent()
+guard let nsimg1 = NSImage(contentsOf: img1Path) else { throw PixelMatchCommandLineError.fileDoesNotExist(url: img1Path) }
+let img1 = JSTPixelImage(nsImage: nsimg1)
+guard let nsimg2 = NSImage(contentsOf: img2Path) else { throw PixelMatchCommandLineError.fileDoesNotExist(url: img2Path) }
+let img2 = JSTPixelImage(nsImage: nsimg2)
 
 guard img2.size.width == img1.size.width && img2.size.height == img1.size.height else {
-    print("Image dimensions do not match: \(img1.size.width)x\(img1.size.height) vs \(img2.size.width)x\(img2.size.height)")
-    exit(EXIT_FAILURE)
+    throw PixelMatchCommandLineError.sizesDoNotMatch(size1: img1.size, size2: img2.size)
 }
 
-let totalWidth = Int(img1.size.width)
-, totalHeight = Int(img1.size.height)
-, totalCount = totalWidth * totalHeight
-var threadCount: Int = 1
-for tCount in stride(from: 32, to: 1, by: -1) {
-    if totalHeight % tCount == 0 {
+let totalColumns = Int(img1.size.width)
+, totalRows = Int(img1.size.height)
+, totalCount = totalColumns * totalRows
+
+
+// MARK: - Concurrent Perform
+
+var threadCount: Int = 32
+let upperBound = Int(ceil(sqrt(Double(totalRows))))
+for tCount in stride(from: upperBound, to: 1, by: -1) {
+    if totalRows % tCount == 0 {
         threadCount = tCount
         break
     }
 }
-
-let partialHeight = Int(img1.size.height / CGFloat(threadCount))
-, partialCount = totalWidth * partialHeight
+let partialRows = totalRows / threadCount
+, partialCount = totalColumns * partialRows
 
 var diffCount: Int = 0
 var threadOutputs: [Int: [JST_COLOR]] = [:]
+let threadQueue = DispatchQueue(label: "SyncQueue")
 
-var offset = 0
-let syncQueue = DispatchQueue(label: "SyncQueue")
 DispatchQueue.concurrentPerform(iterations: threadCount) { (idx) in
-    var threadOutputs0 = [JST_COLOR](repeating: JST_COLOR(the_color: 0), count: partialCount)
-    var a32 = Array(UnsafeBufferPointer(start: img1.internalPointer.pointee.pixels + idx * partialCount, count: partialCount))
-    var b32 = Array(UnsafeBufferPointer(start: img2.internalPointer.pointee.pixels + idx * partialCount, count: partialCount))
+    
+    let processCount = includeAA || threadCount == 1 ? partialCount : (idx == 0 || idx == threadCount - 1 ? partialCount + totalColumns * 2 : partialCount + totalColumns * 4)
+    let processRows  = includeAA || threadCount == 1 ? partialRows  : (idx == 0 || idx == threadCount - 1 ? partialRows  + 2                : partialRows  + 4)
+    let rowOffset    = includeAA || idx == 0         ? 0            : totalColumns * 2
+    
+    let beginOffset  = idx * partialCount - rowOffset
+    let endOffset    = beginOffset + processCount
+    if includeAA {
+        debugPrint("thread#\(idx): process[\(beginOffset)..<\(endOffset)](\(processCount))")
+    }
+    else {
+        let validBegin   = beginOffset + rowOffset
+        let validEnd     = validBegin  + partialCount
+        debugPrint("thread#\(idx): process[\(beginOffset)..<\(endOffset)](\(processCount))->valid[\(validBegin)..<\(validEnd)](\(partialCount))")
+    }
+    
+    var threadOutputs0 = [JST_COLOR](
+        repeating: JST_COLOR(the_color: 0),
+        count: processCount
+    )
+    var a32 = Array(UnsafeBufferPointer(
+        start: img1.internalPointer.pointee.pixels + beginOffset,
+        count: processCount
+    ))
+    var b32 = Array(UnsafeBufferPointer(
+        start: img2.internalPointer.pointee.pixels + beginOffset,
+        count: processCount
+    ))
+    
     let diffCount0 = try! PixelMatch(
         a32: &a32,
         b32: &b32,
         output: &threadOutputs0,
-        width: totalWidth,
-        height: partialHeight,
+        width: totalColumns,
+        height: processRows,
         options: options
     )
-    syncQueue.sync {
-        threadOutputs[idx] = threadOutputs0
+    
+    threadQueue.sync {
+        threadOutputs[idx] = Array(threadOutputs0[rowOffset..<rowOffset + partialCount])
         diffCount += diffCount0
     }
+    
 }
 
 var outputs: [JST_COLOR] = []
 for idx in 0..<threadCount {
     outputs += threadOutputs[idx]!
 }
+assert(outputs.count == totalCount)
 
-print("different pixels: \(diffCount)")
-print(String(format: "error: %.2f%%", Double(diffCount) / Double(totalCount)))
 
+// MARK: - Output Differences
+
+let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+print(String(format: "time elapsed: %.3fs", timeElapsed))
+print(String(format: "approximate count: \(diffCount), difference: %.3f%%", Double(diffCount) / Double(totalCount) * 100.0))
 if diffCount > 0 {
     let img = UnsafeMutablePointer<JST_IMAGE>.allocate(capacity: MemoryLayout<JST_IMAGE>.size)
     img.pointee.orientation = 0
     img.pointee.is_destroyed = 1
-    img.pointee.width = Int32(totalWidth)
-    img.pointee.height = Int32(totalHeight)
+    img.pointee.width = Int32(totalColumns)
+    img.pointee.height = Int32(totalRows)
     img.pointee.pixels = UnsafeMutablePointer(mutating: outputs)
     try JSTPixelImage(internalPointer: img).pngRepresentation().write(to: diffPath)
 }
