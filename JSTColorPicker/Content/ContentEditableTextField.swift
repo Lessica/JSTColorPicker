30fe46c58cba4e009e49954ce15c1a7dc052acbe
i_82 <5410705+Lessica@users.noreply.github.com>
2020-06-24 15:37:49 +0800

    [BugFix] fix foreground color when cell text field become first responder

---
+++ JSTColorPicker/Content/ContentEditableTextField.swift
@@ -0,0 +1,22 @@
+//
+//  ContentEditableTextField.swift
+//  JSTColorPicker
+//
+//  Created by Darwin on 6/24/20.
+//  Copyright © 2020 JST. All rights reserved.
+//
+
+import Cocoa
+
+class ContentEditableTextField: NSTextField {
+
+    override func becomeFirstResponder() -> Bool {
+        textColor = .labelColor
+        return super.becomeFirstResponder()
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        return super.resignFirstResponder()
+    }
+    
+}
