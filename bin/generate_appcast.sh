9df9e3192d2caa8eaab295515cad7ce9383b2786
82Flex <82flex@gmail.com>
2021-12-08 18:07:23 +0800

    Bump version

--- bin/generate_appcast.sh
+++ bin/generate_appcast.sh
@@ -3,12 +3,12 @@
 # Publish JSTColorPickerSparkle
 APP_VERSION=$(xcodebuild -showBuildSettings -target JSTColorPickerSparkle | grep MARKETING_VERSION | tr -d 'MARKETING_VERSION =')
 APP_BUILD_VERSION=$(xcodebuild -showBuildSettings -target JSTColorPickerSparkle | grep CURRENT_PROJECT_VERSION | tr -d 'CURRENT_PROJECT_VERSION =')
-create-dmg --overwrite --identity='Developer ID Application: Zheng Wu (GXZ23M5TP2)' ./releases/Applications/JSTColorPicker.app ./releases/Applications/
-mv ./releases/Applications/*.dmg ./releases/JSTColorPicker_${APP_VERSION}-${APP_BUILD_VERSION}.dmg
+create-dmg --overwrite --identity='Developer ID Application: Zheng Wu (GXZ23M5TP2)' ./releases/apps/JSTColorPicker.app ./releases/apps/
+mv ./releases/apps/*.dmg ./releases/JSTColorPicker_${APP_VERSION}-${APP_BUILD_VERSION}.dmg
 ./Pods/Sparkle/bin/generate_appcast --download-url-prefix https://cdn.82flex.com/jstcpweb/ -o ./releases/appcast.xml ./releases
-scp ./releases/appcast.xml ubuntu@xtzn-raspi.local:/var/www/html/jstcpweb
+scp -P 58422 ./releases/appcast.xml ubuntu@120.55.68.129:/var/www/html/jstcpweb
 
 # Publish JSTColorPickerHelper
 HELPER_VERSION=$(xcodebuild -showBuildSettings -target JSTColorPickerHelper | grep MARKETING_VERSION | tr -d 'MARKETING_VERSION =')
 HELPER_BUILD_VERSION=$(xcodebuild -showBuildSettings -target JSTColorPickerHelper | grep CURRENT_PROJECT_VERSION | tr -d 'CURRENT_PROJECT_VERSION =')
-zip -qr ./releases/Helpers/JSTColorPickerHelper_${HELPER_VERSION}-${HELPER_BUILD_VERSION}.zip ./releases/Helpers/JSTColorPickerHelper.app
+zip -qr ./releases/helpers/JSTColorPickerHelper_${HELPER_VERSION}-${HELPER_BUILD_VERSION}.zip ./releases/helpers/JSTColorPickerHelper.app
