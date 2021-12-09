c8cacbef11b1f7313edf304d230d0eaff83d1c27
82Flex <82flex@gmail.com>
2021-12-09 19:05:03 +0800

    Add Android Support

---
+++ JSTScreenshotHelper/AndroidSupport/AndroidDevice.swift
@@ -0,0 +1,11 @@
+//
+//  AndroidDevice.swift
+//  JSTColorPicker
+//
+//  Created by Rachel on 2021/12/9.
+//  Copyright © 2021 JST. All rights reserved.
+//
+
+import Foundation
+
+final class AndroidDevice: JSTDevice {}
